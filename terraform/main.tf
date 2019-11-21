e8b3704e91d4b8a6eb279221363cdf26e1842ae9
Anders Eknert <anders@eknert.com>
2019-11-21 22:16:32 +0100

    Registry name must be unique

--- terraform/main.tf
+++ terraform/main.tf
@@ -153,19 +153,17 @@ resource "azurerm_managed_disk" "managed_disk_2" {
   }
 }
 
-
 resource "azurerm_container_registry" "acr" {
-  name                     = "registry"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.region
-  sku                      = "Standard"
-  admin_enabled            = false
+  name                = "eknert"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.region
+  sku                 = "Standard"
+  admin_enabled       = false
 
   tags = {
     environment = var.environment
   }
 }
-
 
 resource "azurerm_virtual_machine" "vm" {
   name                  = "test"
