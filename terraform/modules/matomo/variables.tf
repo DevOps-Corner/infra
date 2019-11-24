c98913afdb3207ee734fc9dc37f5065eab4fd86a
Anders Eknert <anders@eknert.com>
2019-11-25 00:00:48 +0100

    Missing variables.tf in modules

---
+++ terraform/modules/matomo/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "Region of deployment"
+  default     = "westeurope"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of environment"
+  default     = "test"
+}
