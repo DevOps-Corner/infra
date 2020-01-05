eea1c7ae9733b60490b8939c92cbf5427de9aab3
Anders Eknert <anders@eknert.com>
2020-01-06 00:05:30 +0100

    Add CDN component

--- pulumi/cdn/cdn.ts
+++ pulumi/cdn/cdn.ts
@@ -6,17 +6,10 @@ const defaultTags = { pulumi: "true" }
 
 const cdnResourceGroup = new azure.core.ResourceGroup("cdn-rg")
 
-const storageAccount = new azure.storage.Account("cdn-sa", {
-    resourceGroupName: cdnResourceGroup.name,
-    accountReplicationType: "LRS",
-    accountTier: "Standard",
-    accountKind: "StorageV2",
-})
-
-const cdnProfile = new azure.cdn.Profile("cdn", {
-    resourceGroupName: cdnResourceGroup.name,
-    sku: "Standard_Microsoft",
-})
+// const cdnProfile = new azure.cdn.Profile("cdn", {
+//     resourceGroupName: cdnResourceGroup.name,
+//     sku: "Standard_Microsoft",
+// })
 
 // const cdnEndpoint = new azure.cdn.Endpoint("cdn", {
 //     origins: [{
@@ -28,3 +21,25 @@ const cdnProfile = new azure.cdn.Profile("cdn", {
 // });
 
 //export const cdnHostname = cdnEndpoint.hostName
+
+export class CDN extends pulumi.ComponentResource {
+    readonly storageAccount: azure.storage.Account
+    readonly container: azure.storage.Container
+
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("eknert:cdn:CDN", name, {}, opts)
+
+        this.storageAccount = new azure.storage.Account(name + "sa", {
+            resourceGroupName: cdnResourceGroup.name,
+            accountReplicationType: "LRS",
+            accountTier: "Standard",
+            accountKind: "StorageV2",
+            tags: defaultTags,
+        })
+
+        this.container = new azure.storage.Container(name + "-container", {
+            containerAccessType: "private",
+            storageAccountName: this.storageAccount.name,
+        })
+    }
+}
